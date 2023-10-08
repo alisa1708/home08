41892781bbaf0820814d99fe17fe9af9a3b7bed3
Алиса Сидорчик <sidorchik@sfedu.ru>
2023-10-08 15:28:22 +0300

    Я написала программу,которая спрашивает у пальзователя язык,на котором он говорит и выводит приветствие на этом языке

--- 10.pas
+++ 10.pas
@@ -1,3 +1,7 @@
 ﻿begin
-  print('n')
+  Print('На каком языке вы говорите?');
+  var N:=ReadString('');
+  if N='На русском' then Print('Привет!');
+  if N='На английском' then Print('Hello!');
+  if N='На немецком' then Print('Servus!');
 end.
